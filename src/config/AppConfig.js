6bb5c0a6a742a1ce04896d75645982fa5f8d8d2d
tim <tim@cnblogs.com>
2017-11-29 15:04:27 +0800

    移除APP配置

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,9 +1,9 @@
 class AppConfig{
     //clientId和clientSecret到https://oauth.cnblogs.com 申请授权
-    static clientId='dade32c4-0fb2-4cef-8b2f-85df14f1e6e1';
-    static clientSecret='C6FH4OmZ0AV2Gp7FtK2lITL5TAsctNZpII7SkzVDOdXcXVJ3s6KYKoNJp5TuCqUv3LJpqzkyYNCyZBYT';
+    static clientId='';
+    static clientSecret='';
     //codepush部署秘钥
-    static deploymentKey='Na445gxFz-KIIu2j1gGjEmUq4QpZ487f23ee-7ab9-474a-b2ea-308f881eabe4';
+    static deploymentKey='';
     static authorizedUrl='https://oauth.cnblogs.com/connect/token';
 }
 
