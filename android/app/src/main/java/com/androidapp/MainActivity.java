99161995331c955e67db07606f8349d33be56b12
tim <tim@cnblogs.com>
2018-01-02 17:16:11 +0800

    重构代码

--- android/app/src/main/java/com/androidapp/MainActivity.java
+++ android/app/src/main/java/com/androidapp/MainActivity.java
@@ -1,24 +1,9 @@
 package com.androidapp;
 
-import android.graphics.Color;
-import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.bridge.ReactContext;
-import com.mehcode.reactnative.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // Show the js-controlled splash screen
-        // NOTE: It needs the react instance manager so it can listen to the react
-        //       context creation event and hide the native splash
-        SplashScreen.show(this, getReactInstanceManager());
-
-        super.onCreate(savedInstanceState);
-    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
--- android/app/src/main/java/com/androidapp/MainApplication.java
+++ android/app/src/main/java/com/androidapp/MainApplication.java
@@ -3,13 +3,6 @@ package com.androidapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.mehcode.reactnative.splashscreen.SplashScreenPackage;
-import com.microsoft.codepush.react.CodePush;
-import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.psykar.cookiemanager.CookieManagerPackage;
-import com.dscj.autoheightwebview.AutoHeightWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -21,12 +14,6 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-
-        @Override
-        protected String getJSBundleFile() {
-        return CodePush.getJSBundleFile();
-        }
-    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -35,14 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SplashScreenPackage(),
-            new CodePush("Na445gxFz-KIIu2j1gGjEmUq4QpZ487f23ee-7ab9-474a-b2ea-308f881eabe4", getApplicationContext(), BuildConfig.DEBUG),
-            new WebViewBridgePackage(),
-            new VectorIconsPackage(),
-            new RNSpinkitPackage(),
-            new CookieManagerPackage(),
-            new AutoHeightWebViewPackage()
+          new MainReactPackage()
       );
     }
 
