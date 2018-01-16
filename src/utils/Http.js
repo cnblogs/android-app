275010df1d2c26e03797d90370a371d5f14cbfc7
tim <tim@cnblogs.com>
2018-01-16 16:46:04 +0800

    封装axios

---
+++ src/utils/Http.js
@@ -0,0 +1,50 @@
+import axios,{AxiosResponse} from 'axios'
+
+class Http{
+    static async GetAsync(url,access_token){
+        let response=await axios({
+                method:'get',
+                url:url,
+                headers:{
+                    "Authorization":`Bearer ${access_token}`
+                }
+            });
+            return response;      
+    }
+    static async PostAsJsonAsync(url,access_token,data){
+        let response=await axios({
+            method:'post',
+            url:url,
+            data:JSON.stringify(data),
+            headers:{
+                "Authorization":`Bearer ${access_token}`,
+                "Content-Type":"application/json"
+            }
+        });
+        return response;
+    }  
+
+    static async PostAsFormAsync(url,data){
+        let response=await axios({
+            method:'post',
+            url:url,
+            data:data,
+            headers:{
+                "Content-Type":"application/x-www-form-urlencoded"
+            }
+        });
+        return response;
+    }
+    
+    static async DeleteAsync(url,access_token){
+        let response = await axios({
+            method: 'Delete',
+            url:url,
+            headers: {
+                "Authorization": `Bearer ${access_token}`
+            }
+        })
+    }
+}
+
+export default Http;
