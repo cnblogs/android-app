e40191b63a75d41c89dec50d98b58c9fcf3a19b3
tim <tim@cnblogs.com>
2018-01-17 12:05:51 +0800

    设置界面

---
+++ src/page/MyScreen/SettingScreen.js
@@ -0,0 +1,56 @@
+import  * as React from 'react';
+import {Text, AsyncStorage,StyleSheet,View} from 'react-native';
+import {Button} from 'native-base';
+import CookieManager from 'react-native-cookies'
+
+class SettingScreen extends React.Component{
+    static navigationOptions={
+        title:'设置'
+    }
+constructor(){
+    super()
+    this.state={
+        isLogin:false,
+        btnBackgroundColor:'#397af8'
+    }
+}
+async componentWillMount(){
+    const data=await AsyncStorage.getItem('a_token');
+   if(data){
+        this.setState({
+            isLogin:true,
+            btnBackgroundColor:'#EB5F6B'
+        })
+   }
+}
+   async loginout(){
+        //清除cookies
+        CookieManager.clearAll().then((res)=>{
+            console.log("CookieManager.ClearAll is Ok")
+          })
+        //清除token
+       await AsyncStorage.removeItem('a_token')
+       this.props.navigation.navigate('Mime')
+    }
+    render(){
+        return(
+            <View style={styles.container}>
+              <Button
+                 backgroundColor={this.state.btnBackgroundColor}
+                 color='white'
+                 disabled={!this.state.isLogin}
+                 onPress={()=>this.loginout()}
+            ><Text>退出登录</Text></Button>
+            </View>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+    container:{
+        marginTop:50,
+        flex:1
+    }
+})
+
+export default SettingScreen;
