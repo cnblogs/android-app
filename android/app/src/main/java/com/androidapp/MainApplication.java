ae49d0ddeaae85ec8c3e696718f78fbe74bf9c45
tim <tim@cnblogs.com>
2018-01-18 10:03:17 +0800

    //

--- android/app/src/main/java/com/androidapp/MainApplication.java
+++ android/app/src/main/java/com/androidapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.androidapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.psykar.cookiemanager.CookieManagerPackage;
+import com.dscj.autoheightwebview.AutoHeightWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new CookieManagerPackage(),
+            new AutoHeightWebViewPackage()
       );
     }
 
